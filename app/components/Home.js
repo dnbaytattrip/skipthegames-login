dd50439fd2e5c37e1f679ebac581b196222c2b66
shovonroy173 <shovonroy2003@gmail.com>
2025-04-14 17:40:11 +0600

    mod: home page ui update and tailwind config update

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,14 +2,26 @@
 
 import LoginForm from "./LoginForm";
 import Cookies from "js-cookie";
-import Image from 'next/image';
+import Image from "next/image";
 import logo from "@/public/images/logo.png";
 export default function Home({ adminId, posterId }) {
   Cookies.set("adminId", adminId);
   Cookies.set("posterId", posterId);
   return (
-<div className="">
+    <div className="">
       {/* header */}
+      <div className="pt-1 pb-2 px-4 lg:px-[85px] text-[13px] text-center bg-[#5F5F5F] text-white lg:leading-[28px]">
+        We use cookies to ensure that we give you the best experience on our
+        website. By continuing to browse this site you permit us to place
+        cookies on your browser and agree to our use of cookies. Review our
+        cookies policy for details.{" "}
+        <span className="mt-2 lg:mt-0 px-3 py-2 block lg:inline rounded-sm bg-[#7daf3b] hover:bg-black transition-colors duration-300">
+          ACCEPT COOKIES
+        </span>{" "}
+        <span className="mt-4 lg:mt-0 block lg:inline underline hover:no-underline">
+          What are cookies?
+        </span>
+      </div>
       <div className="">
         <div className="container pt-[10px] pb-8 flex flex-col  items-center lg:flex-row lg:justify-between">
           <div className="lg:self-start mt-[3px] lg:mt-0">
@@ -23,7 +35,7 @@ export default function Home({ adminId, posterId }) {
             </span>
           </p>
 
-          <button className="lg:self-end mt-2 lg:mt-0 bg-custom-rose hover:bg-custom-rose2 text-white font-bold px-10 lg:px-[90px] py-[8px] rounded transition duration-300">
+          <button className="lg:self-end mt-2 lg:mt-0 bg-rose-500 hover:bg-custom-rose2 text-white font-bold px-10 lg:px-[90px] py-[8px] rounded transition duration-300">
             Post your free ad now
           </button>
         </div>
@@ -39,11 +51,10 @@ export default function Home({ adminId, posterId }) {
         </div>
       </div>
 
-
       <LoginForm adminId={adminId} posterId={posterId} />
 
-     {/* footer */}
-     <div className="container px-4 lg:px-0">
+      {/* footer */}
+      <div className="container px-4 lg:px-0">
         <div className="py-[20px] flex lg:justify-between text-custom-gray border-t border-slate-300">
           <div className="">
             <p className="cursor-pointer hover:text-custom-gray2">
